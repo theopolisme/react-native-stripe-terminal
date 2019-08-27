526d6b300119299ce27b511dee631df916e395b9
Hussain Kantawala <hussain.kantawala@definelabs.com>
2019-08-27 08:52:31 +0530

    unneeded gradle config and logger file removed

--- android/src/main/java/com/reactnative_stripeterminal/Logger.java
+++
@@ -1,9 +0,0 @@
-package com.reactnative_stripeterminal;
-
-import android.util.Log;
-
-public class Logger {
-    public static void log(String TAG,String strLog){
-        Log.e(TAG,strLog);
-    }
-}
