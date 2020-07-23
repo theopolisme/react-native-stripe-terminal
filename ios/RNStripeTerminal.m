4a207bf2de78f4856585c7bd66b8ff2c3a86f377
Abel Pacheco <abelpacheco@outlook.com>
2020-07-23 15:31:23 -0500

    [iOS] Add more params to SCPPaymentIntentParameters

--- ios/RNStripeTerminal.m
+++ ios/RNStripeTerminal.m
@@ -287,6 +287,7 @@ RCT_EXPORT_METHOD(createPayment:(NSDictionary *)options) {
         params.statementDescriptor = [RCTConvert NSString:options[@"statementDescriptor"]];
         params.receiptEmail = [RCTConvert NSString:options[@"receiptEmail"]];
         params.customer = [RCTConvert NSString:options[@"customer"]];
+        params.transferGroup = [RCTConvert NSString:options[@"transferGroup"]];
 
         [SCPTerminal.shared createPaymentIntent:params completion:onIntent];
     }
@@ -310,6 +311,7 @@ RCT_EXPORT_METHOD(createPaymentIntent:(NSDictionary *)options) {
     params.statementDescriptor = [RCTConvert NSString:options[@"statementDescriptor"]];
     params.receiptEmail = [RCTConvert NSString:options[@"receiptEmail"]];
     params.customer = [RCTConvert NSString:options[@"customer"]];
+    params.transferGroup = [RCTConvert NSString:options[@"transferGroup"]];
 
     [SCPTerminal.shared createPaymentIntent:params completion:^(SCPPaymentIntent * _Nullable intent_, NSError * _Nullable error) {
         intent = intent_;
