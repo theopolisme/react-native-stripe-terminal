2281c1dd2153c35ab9dcbc245e64674d0772c70d
jamiesunderland <jamie@Jamies-MacBook-Pro-2.local>
2021-04-13 22:41:37 -0700

    [jrs] fix missing magic method

--- index.js
+++ index.js
@@ -70,6 +70,7 @@ class StripeTerminal {
       'didChangePaymentStatus',
       'didChangeConnectionStatus',
       'didReportUnexpectedReaderDisconnect',
+      'didBeginWaitingForReader'
     ]);
   }
 
