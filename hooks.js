b9d80ed092a6365ad2633b5495a92e93a1cd0b1e
jamiesunderland <jamie@jamies-mbp-2.attlocal.net>
2021-04-13 23:40:21 -0700

    reverting

--- hooks.js
+++ hooks.js
@@ -31,7 +31,7 @@ export default function createHooks(StripeTerminal) {
 
       // Cleanup: remove listeners
       return () => {
-        listeners.forEach(l => l.removeListener());
+        listeners.forEach(l => l.remove());
       };
     }, []);
 
@@ -153,7 +153,7 @@ export default function createHooks(StripeTerminal) {
 
       // Cleanup: remove listeners
       return () => {
-        listeners.forEach(l => l.removeListener());
+        listeners.forEach(l => l.remove());
       };
     }, []);
 
