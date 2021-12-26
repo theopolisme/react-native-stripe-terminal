a9b0040ec401fc1e7af0fe08ea139e8d36af4464
Theo Patt <theo@theopatt.com>
2021-12-26 15:34:47 -0600

    Remove storage change

--- connectionService.js
+++ connectionService.js
@@ -1,5 +1,5 @@
 import EventEmitter from "eventemitter3";
-import { AsyncStorage } from "@react-native-community/async-storage";
+import { AsyncStorage } from "react-native";
 
 export default function createConnectionService(StripeTerminal, options) {
   class STCS {
